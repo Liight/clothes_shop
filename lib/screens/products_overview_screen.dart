8f50fcd5f653d6fcd165066cb5da0d59450811ce
Liight <Liight@users.noreply.github.com>
2020-03-11 08:27:57 +1300

    -update product list logic to handle returning favourites or all -update products overview to implement new product list logic from appbar dropdown

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,7 +1,9 @@
 // flutter
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 // custom
 import '../widgets/products_grid.dart';
+import '../providers/products.dart';
 
 enum FilterOptions {
   Favourites,
@@ -9,17 +11,24 @@ enum FilterOptions {
 }
 
 class ProductsOverviewScreen extends StatelessWidget {
+  
   ProductsOverviewScreen({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final productsContainer = Provider.of<Products>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('My Shop'),
         actions: <Widget>[
           PopupMenuButton(
             onSelected: (FilterOptions selectedValue) {
-              
+              if(selectedValue == FilterOptions.Favourites){
+                productsContainer.showFavouritesOnly();
+              } else {
+                productsContainer.showAll();
+              }
+
             },
             icon: Icon(Icons.more_vert),
             itemBuilder: (_) => [
