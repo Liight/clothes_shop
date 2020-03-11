83b68318a195ba4cb2cafe7cfa9a4b79e6ee7d87
Liight <Liight@users.noreply.github.com>
2020-03-11 16:28:20 +1300

    -create app drawer -register app drawer routes -implement app drawer navigation -minor styles

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,11 @@ import 'package:provider/provider.dart';
 import './screens/cart_screen.dart';
 import './screens/products_overview_screen.dart';
 import './screens/product_detail_screen.dart';
+import './screens/orders_screen.dart';
 import './providers/products.dart';
 import './providers/cart.dart';
 import './providers/orders.dart';
+
 
 // run app
 void main() => runApp(MyApp());
@@ -38,6 +40,7 @@ class MyApp extends StatelessWidget {
         routes: {
           ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
           CartScreen.routeName: (ctx) => CartScreen() ,
+          OrdersScreen.routeName: (ctx) => OrdersScreen(),
         },
       ),
     );
