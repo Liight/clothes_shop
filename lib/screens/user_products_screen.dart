2e3c096b7f54a8005c28d67bf4939b67ca92d014
Liight <Liight@users.noreply.github.com>
2020-03-12 10:22:20 +1300

    -create userProductsScreen

---
+++ lib/screens/user_products_screen.dart
@@ -0,0 +1,34 @@
+// flutter
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+// custom
+import '../providers/products.dart';
+
+class UserProductsScreen extends StatelessWidget {
+  // Shows a list of all the products of the user
+  // Later after authentication filter products by user
+  @override
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<Products>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Products'),
+        actions: <Widget>[
+          IconButton(
+              icon: const Icon(Icons.add),
+              onPressed: () {
+                // Navigator.of(context).pushNamed(routeName);
+              })
+        ],
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(8),
+        child: ListView.builder(
+          itemCount: productsData.items.length,
+          itemBuilder: (ctx, i) => Text('List Items'),
+        ),
+      ),
+    );
+  }
+}
