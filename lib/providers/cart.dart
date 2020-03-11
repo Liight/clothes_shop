8f7af9f5d23d60bf9c183024cade002a8997c5d9
Liight <Liight@users.noreply.github.com>
2020-03-11 13:36:03 +1300

    -add getter for totalAmount

--- lib/providers/cart.dart
+++ lib/providers/cart.dart
@@ -26,6 +26,15 @@ class Cart with ChangeNotifier {
     // should return sum of all quantities
   }
 
+  // placeholder simple maths for the total amount in the cart
+  double get totalAmount {
+    double total = 0.0;
+    _items.forEach((key, cartItem)=>{
+      total += cartItem.price * cartItem.quantity
+    });
+    return total;
+  }
+
   void addItem(
     String productId,
     double price,
