d0541f621d775100ac1e6899517b85b8da7c1571
Liight <Liight@users.noreply.github.com>
2020-03-11 13:38:04 +1300

    - register cartScreen routing

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -6,6 +6,8 @@ import 'package:flutter/material.dart';
 import '../providers/cart.dart';
 
 class CartScreen extends StatelessWidget {
+  static const routeName = '/cart';
+
   @override
   Widget build(BuildContext context) {
     final cart = Provider.of<Cart>(context); // provides access to the cart
