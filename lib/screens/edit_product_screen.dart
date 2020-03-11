93e8d63f896b164a851f85604c651c345cc24222
Liight <Liight@users.noreply.github.com>
2020-03-12 12:17:13 +1300

    -implement form next focus logic

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -1,30 +1,40 @@
 import 'package:flutter/material.dart';
 
 class EditProductScreen extends StatefulWidget {
-  static const routeName ='./edit-product';
+  static const routeName = './edit-product';
   @override
   _EditProductScreenState createState() => _EditProductScreenState();
 }
 
 class _EditProductScreenState extends State<EditProductScreen> {
+final _priceFocusNode = FocusNode();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Edit Product'),
-
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Form(
-          child: ListView(children: <Widget>[
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: 'Title'
+          child: ListView(
+            children: <Widget>[
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Title'),
+                textInputAction: TextInputAction.next, // Icon
+                onFieldSubmitted: (value) {
+                  FocusScope.of(context).requestFocus(_priceFocusNode); // Refocus request 1
+                },
               ),
-              textInputAction: TextInputAction.next,
-            ),
-          ],),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Price'),
+                textInputAction: TextInputAction.next,
+                keyboardType: TextInputType.number,
+                focusNode: _priceFocusNode, // Focus 1 address
+              ),
+            ],
+          ),
         ),
       ),
     );
