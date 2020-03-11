29c5c637fe4a6011992a44a46c64b65a9c93ff2b
Liight <Liight@users.noreply.github.com>
2020-03-12 10:01:12 +1300

    -implement snackbar action

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -60,12 +60,13 @@ class ProductItem extends StatelessWidget {
                 product.title,
               );
               // Show snackbar on main screen when item is added to cart
+              Scaffold.of(context).hideCurrentSnackBar();
               Scaffold.of(context).showSnackBar(SnackBar(
                 content: Text(
                   'Added item to cart',
                 ),
                 duration: Duration(seconds: 2),
-                action: SnackBarAction(label: 'UNDO', onPressed: (){}),
+                action: SnackBarAction(label: 'UNDO', onPressed: () => cart.removeSingleItem(product.id),),
               ));
             },
           ),
